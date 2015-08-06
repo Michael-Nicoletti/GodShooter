d27ed3c411c49e857e9d9d41d93b28bb4fbcd690
Michael Nicoletti <mike.e.nicoletti@gmail.com>
2015-08-06 19:24:29 -0400

    Menu Systems
    Started Adding Menu Systems to the game with raycasts and colliders,
    still needs to navigate.

---
+++ Assets/UI/Menus/mouseControl.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mouseControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            mouseHandler();
+        }
+	
+	}
+
+    void mouseHandler()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+        if (hit.collider != null)
+        {
+            if (hit.collider.name == "PlayButton")
+            {
+                Debug.Log("PLAY BALL");
+            }
+
+            if (hit.collider.name == "Controls")
+            {
+                Debug.Log("DO THE CONTROLS");
+            }
+
+            if (hit.collider.name == "Quit")
+            {
+                Debug.Log("GTFO");
+            }
+            if(hit.collider.name == "returnToMenu")
+            {
+                Debug.Log("GO BACK");
+            }
+        }
+    }
+}
