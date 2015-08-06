b202ac001d693a9c0edd196d215e1fe39fdbf01c
Michael Nicoletti <mike.e.nicoletti@gmail.com>
2015-08-06 11:01:36 -0400

    Initial Game Commit
    Added basic movement and placeholder sprites to game for testing and
    prototyping.

---
+++ Assets/playerMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMovement : MonoBehaviour {
+
+    [SerializeField] int speed;
+
+    private float playerInput;
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+
+        rb = GetComponent<Rigidbody2D>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        playerInput = Input.GetAxisRaw("Horizontal");
+
+        rb.AddForce(Vector2.right * speed * playerInput);
+
+
+	
+	}
+}
