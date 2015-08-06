a00802c5d5598492eb8fe00dae19841ac40bc3c0
Michael Nicoletti <mike.e.nicoletti@gmail.com>
2015-08-06 14:17:56 -0400

    Shooting Update
    Jumping is implemented but needs tweaking, shooting is implemented but
    bullets are not flying in the correct direction. No idea why.

---
+++ Assets/projectileBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class projectileBehaviour : MonoBehaviour {
+
+    private Rigidbody2D rb;
+
+    [SerializeField] int bulletSpeed = 10;
+
+	// Use this for initialization
+	void Start () {
+
+
+        rb = GetComponent<Rigidbody2D>();
+
+
+        rb.AddForce(transform.right * bulletSpeed);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
