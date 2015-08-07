db37ca3d2bb1751841c40bbe0e08a2b1e6dc3aef
Zachary Truckel <zachary.truckel@rcc.yorkvilleu.com>
2015-08-07 19:26:11 -0400

    Timer!!!

--- Assets/ZachsStuff/RoundTimer.cs
+++ Assets/ZachsStuff/RoundTimer.cs
@@ -2,22 +2,48 @@
 using System.Collections;
 
 public class RoundTimer : MonoBehaviour {
-    public float timer = 180.0f;
+    public float timer = 120.0f;
+    public int rounds = 3;
+    public bool roundEnded = false;
 
+    private float timeBeforeNextRound = 5.0f;
+    private float originalTimeBeforeNextRound;
+    private float originalTimer;
+    private GameObject[] players;
 	// Use this for initialization
 	void Start () {
-	    
+        originalTimer = timer;
+        originalTimeBeforeNextRound = timeBeforeNextRound;
+
+        players = GameObject.FindGameObjectsWithTag("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
         Debug.Log("Time: " + timer/60);
 
-        timer -= Time.deltaTime;
-
-        if (timer <= 0)
+        if (!roundEnded)
         {
-            Time.timeScale = 0;
+            timer -= Time.deltaTime;
+        }
+        
+        if (roundEnded && rounds > 0)
+        {
+            timeBeforeNextRound -= Time.deltaTime;
+        }
+
+        if (timer < 0)
+        {
+            roundEnded = true;
+            rounds--;
+
+            timer = originalTimer;
+        }
+        if (timeBeforeNextRound < 0)
+        {
+            roundEnded = false;
+
+            timeBeforeNextRound = originalTimeBeforeNextRound;
         }
 	}
 }
