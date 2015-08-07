7e44b0c1cd3bcefc16645eaf9efd51aca501fe93
Zachary Truckel <zachary.truckel@rcc.yorkvilleu.com>
2015-08-07 18:38:06 -0400

    Death!!!

---
+++ Assets/ZachsStuff/PlayerDeath.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDeath : MonoBehaviour {
+    public bool dead = false;
+
+    private playerMovement healthScript;
+    private int playerHealth;
+
+	// Use this for initialization
+	void Start () {
+        healthScript = GetComponent<playerMovement>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        playerHealth = healthScript.getHp();
+
+        if (playerHealth == 0)
+        {
+            dead = true;
+
+            gameObject.SetActive(false);
+        }
+	}
+}
---
+++ Assets/ZachsStuff/RoundTimer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoundTimer : MonoBehaviour {
+    public float timer = 180.0f;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("Time: " + timer/60);
+
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            Time.timeScale = 0;
+        }
+	}
+}
