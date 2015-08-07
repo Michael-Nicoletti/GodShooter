31c5d97a29fddac47daa5186f448fd8da201426e
Zachary Truckel <zachary.truckel@rcc.yorkvilleu.com>
2015-08-07 09:08:30 -0400

    More Gravity!!!

--- Assets/ZachsStuff/GravityManipulation.cs
+++ Assets/ZachsStuff/GravityManipulation.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class GravityManipulation : MonoBehaviour {
     private GameObject[] players;
+    private float gravityChange;
 
 	// Use this for initialization
 	void Start () {
@@ -11,7 +12,7 @@ public class GravityManipulation : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    
+        gravityChange = Random.Range(-2, 2);
 	}
 
     void OnTriggerEnter2D(Collider2D c)
@@ -19,12 +20,10 @@ public class GravityManipulation : MonoBehaviour {
         if (c.gameObject == players[0] || c.gameObject == players[1] || c.gameObject == players[2] || c.gameObject == players[3])
         {
             Debug.Log("Gravity Manipulated");
-            float gravityChange = Random.Range(-1, 1);
 
             Physics2D.gravity += new Vector2(0, gravityChange);
 
             Debug.Log(Physics2D.gravity);
         }
-    
     }
 }
