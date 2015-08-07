86284c14a558900efcfac603db855efa7d91c029
Zachary Truckel <zachary.truckel@rcc.yorkvilleu.com>
2015-08-07 09:46:08 -0400

    Player Flips!!!

--- Assets/ZachsStuff/GravitySwitch.cs
+++ Assets/ZachsStuff/GravitySwitch.cs
@@ -24,6 +24,18 @@ public class GravitySwitch : MonoBehaviour {
             Physics2D.gravity = -Physics2D.gravity;
 
             Debug.Log(Physics2D.gravity);
+
+            for (int i = 0; i < players.Length; i++)
+            {
+                if (players[i].transform.rotation.z == 0)
+                {
+                    players[i].transform.rotation = Quaternion.Slerp(players[i].transform.rotation, Quaternion.Euler(0, 0, 180), 2.0f);
+                }
+                else
+                {
+                    players[i].transform.rotation = Quaternion.Slerp(players[i].transform.rotation, Quaternion.Euler(0, 0, 0), 2.0f);
+                }
+            }
         }
     }
 }
